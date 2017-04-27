010447b8536be3749583b3ea59f2ba7b032e9bce
Jakub Chronowski <jakub@chronow.ski>
2017-04-27 16:54:59 +0800

    Substract time to start instead of adding it.

--- src/countup.js
+++ src/countup.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react'
 import convertTime from './convert-time'
 
 class CountUp extends Component {
-  constructor () {
-    super()
-    this.startTime = Date.now() + (props.startTime * 1000)
-    this.state = { time: 0 }
+  constructor (props) {
+    super(props)
+    this.startTime = Date.now() - (props.startTime * 1000)
+    this.state = { time: props.startTime }
     this.tick = this.tick.bind(this)
   }
 
